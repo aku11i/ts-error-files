3a9b2c23b52d0e9fc94d458ed8adbf4d3fdb7169
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2023-06-07 23:59:07 +0900

    chore: Add e2e tests

---
+++ e2e.test.ts
@@ -0,0 +1,29 @@
+import { describe, it } from "node:test";
+import { execSync } from "child_process";
+import assert from "assert";
+
+describe("tsc-error-files", () => {
+  it("should print nothing when tsc is succeeded", () => {
+    const command = `npm --silent run dev -- --config test-cases/no-errors/tsconfig.json`;
+
+    const result = execSync(command).toString();
+
+    const expected = "";
+
+    assert.strictEqual(result, expected);
+  });
+
+  it("should print error files when tsc detects any errors", () => {
+    const command = `npm --silent run dev -- --config test-cases/has-errors/tsconfig.json`;
+
+    const result = execSync(command).toString();
+
+    const expected =
+      [
+        "/Users/main/ghq/github.com/aku11i/tsc-error-files/test-cases/has-errors/type-error.ts",
+        "/Users/main/ghq/github.com/aku11i/tsc-error-files/test-cases/has-errors/syntax-error.ts",
+      ].join("\n") + "\n";
+
+    assert.strictEqual(result, expected);
+  });
+});
